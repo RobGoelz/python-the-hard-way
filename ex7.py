1246c0547ae86413ca8016b96ca9ba83150ffcdc
Rob Goelz <rgoelz@rgoelz-linuxws.localdomain>
2016-07-19 16:42:10 -0400

    Add comments to example 7

--- ex7.py
+++ ex7.py
@@ -1,21 +1,21 @@
-print "Mary had a little lamb."
-print "Its fleece was white as %s." % 'snow'
-print "And everywhere that Mary went."
-print "." * 10 #what'd that do?
+print "Mary had a little lamb." # prints the string in ""
+print "Its fleece was white as %s." % 'snow' # prints the string in "" and calls the python format operator for strings %s to accept the string 'snow' from outside of the ""
+print "And everywhere that Mary went." # prints the string ""
+print "." * 10 #what'd that do? # prints . 10 times - literally runs the print command times ten
 
-end1 = "C"
-end2 = "h"
-end3 = "e"
-end4 = "e"
-end5 = "s"
-end6 = "e"
-end7 = "B"
-end8 = "u"
-end9 = "r"
-end10 = "g"
-end11 = "e"
-end12 = "r"
+end1 = "C" # variable assignment
+end2 = "h" # variable assignment
+end3 = "e" # variable assignment
+end4 = "e" # variable assignment
+end5 = "s" # variable assignment
+end6 = "e" # variable assignment
+end7 = "B" # variable assignment
+end8 = "u" # variable assignment
+end9 = "r" # variable assignment
+end10 = "g" # variable assignment
+end11 = "e" # variable assignment
+end12 = "r" # variable assignment
 
-# watch that comma at the end of the nest line
+# concatenates the variables above to make two words: Cheese Burger
 print end1 + end2 + end3 + end4 + end5 + end6, # <- this comma prints both strings on the same line, otherwise it prints the next print statement on the following line
 print end7 + end8 + end9 + end10 + end11 + end12
