e87b6ba50204e2903c9354be4a60f7c2b814fcab
Rob Goelz <RobGoelz@gmail.com>
2016-07-22 11:57:09 -0400

    Add example 13 python script to repo, with comments.

--- ex13.py
+++ ex13.py
@@ -6,14 +6,17 @@ from sys import argv
 # below we see the purpose of the arg variable, which is to
 # hold the arguments you pass to a python script when you
 # run the python script
-# here we're passing four arguments to the agrv variable
+
+# here we're essentially unpacking the arguments held in 
+# argv and passing the contents to four varaibles, in order,
+# from left to right:
 script, first, second, third = argv
 
-# prints the "script" argument
+# prints the "script" variable assigned from argv
 print "The script is called:", script
-# prints the "first" argument
+# prints the "first" variable assigned from argv
 print "The first variable is:", first
-# prints the "second" argument
+# prints the "second" variable assigned from argv
 print "The second variable is:," second
-# prints the "third" argument
+# prints the "third" variable assigned from argv
 print "The third varaible is:," third
