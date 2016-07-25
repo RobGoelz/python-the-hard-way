bb6e8b8cd671cb295fca7964e88b72e685324679
Rob Goelz <RobGoelz@gmail.com>
2016-07-25 11:34:02 -0400

    Add example 15 to repo, with comments.

---
+++ ex15.py
@@ -0,0 +1,32 @@
+# calls the argv (argument) variable from the python sys module
+from sys import argv
+
+# assigns the initial command line input
+script, filename = argv
+
+# opens the file using the filename variable provided by the
+# initial command line argument (though argv)
+# and assigns the contents of the file to the variable "txt"
+txt = open (filename)
+
+# prints the string & raw output of the filename varaible
+print "Here's your file %r:" % filename
+# prints the contents of the text variable by passing it to 
+# the read function "read()"
+print txt.read()
+
+# prints string to prompt for filename
+print "Type the filename again:"
+
+# takes in a filename from the command prompt using raw_input
+# and the "> " characters to represent a prompt and assigns
+# the file to a variable called file_again
+file_again = raw_input("> ")
+
+# opens the file specified by the file_again varaible
+# assigns the contents of the file to the txt_again varaible
+txt_again = open(file_again)
+
+# prints the contents of the txt_again variable by passing it 
+# to the read function "read()"
+print txt_again.read()
